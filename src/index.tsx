d10c46a388e10605d24cda95dea09943915a8b7e
Arthur <arthur.gamblin.dev@gmail.com>
2019-05-01 17:28:34 +0200

    fixed smart-imporst and ci

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@/App';
 
-ReactDOM.render(
-  <div>
-    <App />
-  </div>,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
