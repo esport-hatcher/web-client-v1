2382d0324bb9eb7b1dafb4e0b3f1e355fef6c21a
Arthur <arthur.gamblin@epitech.eu>
2019-09-19 21:01:13 +0200

    refacto(useToggler): onChange now use a useCallback

--- src/app/custom-hooks/useToggler.tsx
+++ src/app/custom-hooks/useToggler.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 export const useToggler = (
     initialValue: boolean = false
 ): [boolean, () => void] => {
     const [value, setValue] = useState(initialValue);
 
-    const onChange = () => setValue(!value);
+    const onChange = useCallback(() => setValue(value => !value), [setValue]);
     return [value, onChange];
 };
