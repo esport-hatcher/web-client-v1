29269a94ccab87db76506b548f5a24b8f95589b9
Arthur <arthur.gamblin@epitech.eu>
2019-09-22 00:35:46 +0200

    fix(redux): store is reseted on logout

--- src/app/config/Root.tsx
+++ src/app/config/Root.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from '@/reducers';
+import { rootReducer } from '@/reducers';
 import { Provider } from 'react-redux';
 
 declare global {
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,13 +1,24 @@
 import { combineReducers } from 'redux';
 import { authenticationReducer, IAuthentication } from './authentication';
 import { registerFormReducer, IRegisterFormReducer } from './registerForm';
+import { ActionTypes } from '@/actions';
 
 export interface IStoreState {
     authentication: IAuthentication;
     registerForm: IRegisterFormReducer;
 }
 
-export default combineReducers<IStoreState>({
+export const appReducer = combineReducers<IStoreState>({
     authentication: authenticationReducer,
     registerForm: registerFormReducer,
 });
+
+// tslint:disable-next-line: no-any
+export const rootReducer = (state: any, action: any) => {
+    if (action.type === ActionTypes.logout) {
+        // tslint:disable-next-line: no-parameter-reassignment
+        state = undefined;
+    }
+
+    return appReducer(state, action);
+};
