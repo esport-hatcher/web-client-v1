1e8bd2018e84a9daa5bf58cad412a63d9deb9b1b
Arthur <arthur.gamblin@epitech.eu>
2019-12-19 12:30:39 -0500

    WIP(AutoComplete): basic implementation of auto-complete input

---
+++ src/app/components/auto-complete/AutoComplete.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { useInput } from '@/custom-hooks';
+import { SmartInput } from '@/components';
+
+interface IProps {
+    items: string[];
+    // tslint:disable-next-line: no-any
+    onSelect: any;
+}
+
+export const AutoComplete: React.FC<IProps> = ({ items }) => {
+    const [country, onChange] = useInput();
+    const [matchings, setMatchings] = useState<string[]>([]);
+
+    useEffect(() => {
+        // tslint:disable-next-line: no-console
+        const reg = new RegExp('^' + country.toLowerCase() + '.*');
+        setMatchings(items.filter(item => item.toLowerCase().match(reg)));
+    }, [country, items]);
+
+    const displayItems = () => {
+        return matchings.map(item => {
+            return (
+                <li key={item} className='auto-complete__list__item'>
+                    {item}
+                </li>
+            );
+        });
+    };
+
+    const displayList = () => {
+        if (matchings.length > 0) {
+            return <ul className='auto-complete__list'>{displayItems()}</ul>;
+        }
+        return null;
+    };
+
+    return (
+        <div className='auto-complete'>
+            <SmartInput
+                value={country}
+                icon='pen'
+                name='auto-complete'
+                onChange={onChange}
+                placeholder='Country'
+                type='text'
+            />
+            {displayList()}
+        </div>
+    );
+};
--- src/app/screens/Home/HomePage.tsx
+++ src/app/screens/Home/HomePage.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { AutoComplete } from '@/components';
+
+const items = ['France', 'Angleterre', 'Germany', 'Andorre', 'Algérie'];
 
 export const _HomePage: React.FC = () => {
-    return <main>HomePage</main>;
+    const onSelect = useCallback((item: string) => {
+        // tslint:disable-next-line: no-console
+        console.log(item);
+    }, []);
+
+    return (
+        <main>
+            <AutoComplete items={items} onSelect={onSelect} />
+        </main>
+    );
 };
