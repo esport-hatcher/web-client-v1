c73adfb1ae76b7fd7bcd7d83293abaaf769af64e
Arthur <arthur.gamblin@epitech.eu>
2020-01-04 00:30:42 -0500

    feat(AutoComplete): implemented AutoComplete component

--- src/app/components/auto-complete/AutoComplete.tsx
+++ src/app/components/auto-complete/AutoComplete.tsx
@@ -1,34 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import { useInput } from '@/custom-hooks';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useInput, useToggler } from '@/custom-hooks';
 import { SmartInput } from '@/components';
 
 interface IProps {
     items: string[];
-    // tslint:disable-next-line: no-any
-    onSelect: any;
+    onSelect: (selected: string) => void;
 }
 
-export const AutoComplete: React.FC<IProps> = ({ items }) => {
+export const AutoComplete: React.FC<IProps> = ({ items, onSelect }) => {
     const [country, onChange] = useInput();
     const [matchings, setMatchings] = useState<string[]>([]);
+    const [focus, setFocus, setFocusSpecific] = useToggler(false);
 
     useEffect(() => {
-        const reg = new RegExp('^' + country.toLowerCase() + '.*');
-        setMatchings(items.filter(item => item.toLowerCase().match(reg)));
+        if (country.length > 0) {
+            const reg = new RegExp('^' + country.toLowerCase() + '.*');
+            return setMatchings(
+                items.filter(item => item.toLowerCase().match(reg))
+            );
+        }
+        return setMatchings([]);
     }, [country, items]);
+
+    const toggleListOn = useCallback(() => {
+        setFocusSpecific(true);
+    }, [setFocusSpecific]);
+
+    const toggleListOff = useCallback(() => {
+        setTimeout(() => setFocusSpecific(false), 400);
+    }, [setFocusSpecific]);
+
+    const onItemSelect = useCallback(
+        (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+            const itemValue = event.currentTarget.getAttribute('data-key')!;
+            onChange({
+                target: {
+                    value: itemValue,
+                },
+            });
+            onSelect(itemValue);
+            setFocusSpecific(false);
+        },
+        [onSelect, onChange, setFocus]
+    );
 
     const displayItems = () => {
         return matchings.map(item => {
             return (
-                <li key={item} className='auto-complete__list__item'>
-                    {item}
+                <li
+                    key={item}
+                    data-key={item}
+                    className='auto-complete__list__item'
+                    onClick={onItemSelect}
+                >
+                    <p className='important-info important-info--sm auto-complete__list__item__text'>
+                        {item}
+                    </p>
                 </li>
             );
         });
     };
 
+    const isListDsplayable = (): boolean => {
+        if (matchings.length > 0 && focus) {
+            return true;
+        }
+        return false;
+    };
+
     const displayList = () => {
-        if (matchings.length > 0) {
+        if (isListDsplayable()) {
             return <ul className='auto-complete__list'>{displayItems()}</ul>;
         }
         return null;
@@ -43,6 +84,9 @@ export const AutoComplete: React.FC<IProps> = ({ items }) => {
                 onChange={onChange}
                 placeholder='Country'
                 type='text'
+                className={isListDsplayable() ? 'auto-complete__input' : ''}
+                onFocus={toggleListOn}
+                onLoseFocus={toggleListOff}
             />
             {displayList()}
         </div>
