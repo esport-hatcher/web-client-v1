4eae9fdfe7a831d2d8df36622308e573b02a5afb
Arthur <arthur.gamblin@epitech.eu>
2020-01-06 11:11:39 -0500

    Refacto(HomePage): deleted tests components

--- src/app/screens/Home/HomePage.tsx
+++ src/app/screens/Home/HomePage.tsx
@@ -1,17 +1,5 @@
 import React, { useCallback } from 'react';
-import { AutoComplete } from '@/components';
-
-const items = ['France', 'Angleterre', 'Germany', 'Andorre', 'Algérie'];
 
 export const _HomePage: React.FC = () => {
-    const onSelect = useCallback((item: string) => {
-        // tslint:disable-next-line: no-console
-        console.log(item);
-    }, []);
-
-    return (
-        <main>
-            <AutoComplete items={items} onSelect={onSelect} />
-        </main>
-    );
+    return <main>Home</main>;
 };
