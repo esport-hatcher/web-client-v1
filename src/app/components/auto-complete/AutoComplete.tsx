24a0541b6e96abb0accd1eff57078e7a30508ebe
Arthur <arthur.gamblin@epitech.eu>
2020-01-06 09:52:35 -0500

    Refacto(AutoComplete): Fixed typo on variable name + more explicit name for setListDisplayable

--- src/app/components/auto-complete/AutoComplete.tsx
+++ src/app/components/auto-complete/AutoComplete.tsx
@@ -7,88 +7,94 @@ interface IProps {
     onSelect: (selected: string) => void;
 }
 
-export const AutoComplete: React.FC<IProps> = ({ items, onSelect }) => {
-    const [country, onChange] = useInput();
-    const [matchings, setMatchings] = useState<string[]>([]);
-    const [focus, , setFocusSpecific] = useToggler(false);
+export const AutoComplete: React.FC<IProps> = React.memo(
+    ({ items, onSelect }) => {
+        const [country, onChange] = useInput();
+        const [matchings, setMatchings] = useState<string[]>([]);
+        const [listDisplayable, , setListDisplayable] = useToggler(false);
 
-    useEffect(() => {
-        if (country.length > 0) {
-            const reg = new RegExp('^' + country.toLowerCase() + '.*');
-            return setMatchings(
-                items.filter(item => item.toLowerCase().match(reg))
-            );
-        }
-        return setMatchings([]);
-    }, [country, items]);
+        useEffect(() => {
+            if (country.length > 0) {
+                const reg = new RegExp('^' + country.toLowerCase() + '.*');
+                return setMatchings(
+                    items.filter(item => item.toLowerCase().match(reg))
+                );
+            }
+            return setMatchings([]);
+        }, [country, items]);
 
-    const toggleListOn = useCallback(() => {
-        setFocusSpecific(true);
-    }, [setFocusSpecific]);
+        const toggleListOn = useCallback(() => {
+            setListDisplayable(true);
+        }, [setListDisplayable]);
 
-    const toggleListOff = useCallback(() => {
-        setTimeout(() => setFocusSpecific(false), 200);
-    }, [setFocusSpecific]);
+        const toggleListOff = useCallback(() => {
+            setTimeout(() => setListDisplayable(false), 200);
+        }, [setListDisplayable]);
 
-    const onItemSelect = useCallback(
-        (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
-            const itemValue = event.currentTarget.getAttribute('data-key')!;
-            onChange({
-                target: {
-                    value: itemValue,
-                },
+        const onItemSelect = useCallback(
+            (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+                const itemValue = event.currentTarget.getAttribute('data-key')!;
+                onChange({
+                    target: {
+                        value: itemValue,
+                    },
+                });
+                onSelect(itemValue);
+                setListDisplayable(false);
+            },
+            [onSelect, onChange, setListDisplayable]
+        );
+
+        const displayItems = () => {
+            return matchings.map(item => {
+                return (
+                    <li
+                        key={item}
+                        data-key={item}
+                        className='auto-complete__list__item'
+                        onClick={onItemSelect}
+                    >
+                        <p className='important-info important-info--sm auto-complete__list__item__text'>
+                            {item}
+                        </p>
+                    </li>
+                );
             });
-            onSelect(itemValue);
-            setFocusSpecific(false);
-        },
-        [onSelect, onChange, setFocusSpecific]
-    );
+        };
 
-    const displayItems = () => {
-        return matchings.map(item => {
-            return (
-                <li
-                    key={item}
-                    data-key={item}
-                    className='auto-complete__list__item'
-                    onClick={onItemSelect}
-                >
-                    <p className='important-info important-info--sm auto-complete__list__item__text'>
-                        {item}
-                    </p>
-                </li>
-            );
-        });
-    };
+        const isListDisplayable = (): boolean => {
+            if (matchings.length > 0 && listDisplayable) {
+                return true;
+            }
+            return false;
+        };
 
-    const isListDsplayable = (): boolean => {
-        if (matchings.length > 0 && focus) {
-            return true;
-        }
-        return false;
-    };
+        const displayList = () => {
+            if (isListDisplayable()) {
+                return (
+                    <ul className='auto-complete__list'>{displayItems()}</ul>
+                );
+            }
+            return null;
+        };
 
-    const displayList = () => {
-        if (isListDsplayable()) {
-            return <ul className='auto-complete__list'>{displayItems()}</ul>;
-        }
-        return null;
-    };
-
-    return (
-        <div className='auto-complete'>
-            <SmartInput
-                value={country}
-                icon='pen'
-                name='auto-complete'
-                onChange={onChange}
-                placeholder='Country'
-                type='text'
-                className={isListDsplayable() ? 'auto-complete__input' : ''}
-                onFocus={toggleListOn}
-                onLoseFocus={toggleListOff}
-            />
-            {displayList()}
-        </div>
-    );
-};
+        return (
+            <div className='auto-complete'>
+                <SmartInput
+                    value={country}
+                    icon='pen'
+                    name='auto-complete'
+                    onChange={onChange}
+                    placeholder='Country'
+                    type='text'
+                    className={
+                        isListDisplayable() ? 'auto-complete__input' : ''
+                    }
+                    onFocus={toggleListOn}
+                    onLoseFocus={toggleListOff}
+                />
+                {displayList()}
+            </div>
+        );
+    }
+);
