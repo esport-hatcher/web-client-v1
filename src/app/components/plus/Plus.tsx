e3d958bf1cdac4b4f4b08d7b4d58c2c9fda4b74c
Melliti <r.melliti@protonmail.com>
2020-05-11 11:39:53 -0400

    [ADD] team management modal

--- src/app/components/plus/Plus.tsx
+++ src/app/components/plus/Plus.tsx
@@ -1,16 +1,56 @@
 import React from 'react';
+import Modal from '../tm_modal/Modal';
+// ts-ignore
+import ReactModal from 'react-modal';
 
-class Plus extends React.Component {
+class Plus extends React.Component<{}, { showModal: boolean }> {
+    state = {
+        showModal: false,
+    };
+
+    customStyle = {
+        backgroundColor: 'black',
+    };
+
+    constructor(props: boolean) {
+        super(props);
+
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+
+    handleOpenModal = () => {
+        this.setState({ showModal: true });
+    };
+
+    handleCloseModal = () => {
+        this.setState({ showModal: false });
+    };
+
     render() {
         return (
             <div
                 className='card plus'
                 style={{ width: '100px', height: '90px' }}
             >
-                <div className='circle'>
+                <div className='circle' onClick={this.handleOpenModal}>
                     <div className='horizontal'></div>
                     <div className='vertical'></div>
                 </div>
+                <ReactModal
+                    isOpen={this.state.showModal}
+                    contentLabel={'Minimal'}
+                    style={{
+                        overlay: {},
+                        content: {
+                            backgroundColor: '#16212e',
+                        },
+                    }}
+                >
+                    <Modal handleCloseModal={this.handleCloseModal} />
+
+                    {/* <button onClick={this.handleCloseModal}> close modal</button> */}
+                </ReactModal>
             </div>
         );
     }
---
+++ src/app/components/tm_modal/Modal.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+// ts-ignore
+import ReactModal from 'react-modal';
+
+interface IProps {
+    handleCloseModal: () => void;
+}
+
+class Modal extends React.Component<IProps> {
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='modal-container'>
+                <div className='create modal-box'>
+                    <div className='action-container'>
+                        <h1 className='create-title'>Create</h1>
+                        <img
+                            src={process.env.PUBLIC_URL + '/create.png'}
+                            alt='create'
+                            className='modal-image'
+                        />
+                        <div className='button-wrapper'>
+                            <button
+                                onClick={this.props.handleCloseModal}
+                                className='create-button'
+                            >
+                                Create a team
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className='join modal-box'>
+                    <div className='action-container'>
+                        <h1 className='join-title'>Join</h1>
+                        <img
+                            src={process.env.PUBLIC_URL + '/join.png'}
+                            alt='join'
+                            className='modal-image'
+                        />
+                        <div className='button-wrapper'>
+                            <button
+                                onClick={this.props.handleCloseModal}
+                                className='join-button'
+                            >
+                                Join a team
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Modal;
