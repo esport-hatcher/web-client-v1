a968a9464e157b07a6eb1e2c8be55cc86efb2430
Arthur <arthur.gamblin@epitech.eu>
2020-05-31 14:16:27 -0400

    fix(Navigation)
    Refactored conditional classname with CX package

--- src/app/layouts/Navigation/Navigation.tsx
+++ src/app/layouts/Navigation/Navigation.tsx
@@ -32,58 +32,46 @@ export const NavBar: React.FC<IProps> = React.memo(({ admin }) => {
     return (
         <React.Fragment>
             <div
-                className={`nav-bar__placeholder ${
-                    expanded ? 'nav-bar__placeholder--expanded' : ''
-                }`}
+                className={cx('nav-bar__placeholder', {
+                    'nav-bar__placeholder--expanded': expanded,
+                })}
             />
             <nav className={cx('nav-bar', { 'nav-bar--expanded': expanded })}>
                 {admin && (
                     <NavigationItem
-                        // active={currentItem.includes(routesPath.adminPannel)}
                         activeText={textDisplay}
                         Icon={AiOutlineTool}
-                        // onClick={onItemClick}
                         path={routesPath.adminPannel}
                         text='Admin Pannel'
                     />
                 )}
                 <NavigationItem
-                    // active={currentItem.includes(routesPath.chat)}
                     activeText={textDisplay}
                     Icon={AiOutlineMail}
-                    // onClick={onItemClick}
                     path={routesPath.chat}
                     text='Chat'
                 />
                 <NavigationItem
-                    // active={currentItem.includes(routesPath.teams)}
                     activeText={textDisplay}
                     Icon={AiOutlineUser}
-                    // onClick={onItemClick}
                     path={routesPath.teams}
                     text='Teams management'
                 />
                 <NavigationItem
-                    // active={currentItem.includes(routesPath.feed)}
                     activeText={textDisplay}
                     Icon={AiOutlineFire}
-                    // onClick={onItemClick}
                     path={routesPath.feed}
                     text='Feed'
                 />
                 <NavigationItem
-                    // active={currentItem.includes(routesPath.settingsProfile)}
                     activeText={textDisplay}
                     Icon={AiOutlineSetting}
-                    // onClick={onItemClick}
                     path={routesPath.settingsProfile}
                     text='Settings'
                 />
                 <NavigationItem
-                    // active={currentItem.includes(routesPath.logout)}
                     activeText={textDisplay}
                     Icon={AiOutlineLogout}
-                    // onClick={onItemClick}
                     path={routesPath.logout}
                     text='Logout'
                 />
