8bd74d5e7a7f4f6a326d50eec14d9014e66cff39
Arthur <arthur.gamblin@epitech.eu>
2020-06-10 13:59:43 -0400

    fix(TaskReducer)
    - fixed `fetchTaskSuccess` in related reducer

--- src/app/reducers/tasks.ts
+++ src/app/reducers/tasks.ts
@@ -1,11 +1,13 @@
 import { Action, ITask, ActionTypes } from 'app/actions';
+import unionBy from 'lodash/unionBy';
 
 const tasksReducer = (state: ITask[] = [], action: Action) => {
     switch (action.type) {
         case ActionTypes.createTaskSuccess:
             return [...state, action.task];
         case ActionTypes.fetchTaskSuccess:
-            return [...state, ...action.tasks];
+            // merge state and action.tasks while removing duplicates by id
+            return unionBy(state, action.tasks, 'id');
         case ActionTypes.deleteTaskSuccess:
             return state.filter(task => task.id !== action.task.id);
         case ActionTypes.patchTaskSuccess:
