fa4044e3f10db8476c6e86eae6f6fadd9a91378a
Arthur <arthur.gamblin@epitech.eu>
2020-06-25 15:59:37 -0400

    feat(Calendar)
    - exported style for `CalendarCell` into a separated file

--- src/app/components/calendar/Cell.tsx
+++ src/app/components/calendar/Cell.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useCallback } from 'react';
 import format from 'date-fns/format';
 import cx from 'classnames';
-import { IconButton } from '../shared';
+import { IconButton, ModalForm } from '../shared';
 import { AiOutlinePlus, AiFillEye } from 'react-icons/ai';
+import { useToggler } from 'app/custom-hooks';
 
 interface IProps {
     readonly cellDate: Date;
@@ -14,21 +15,20 @@ interface IProps {
 export const CalendarCell: React.FC<IProps> = React.memo(
     ({ cellDate, onClick, disabled = false, selected = false }) => {
         const [isFocus, setIsFocus] = useState(false);
+        const [showModal, toggleModal] = useToggler(false);
 
         const ActionButtons = () => {
             if (isFocus && !disabled) {
                 return (
-                    <div className='calendar__content__cells__row__item__action-buttons'>
+                    <div className='calendar-cell__action-buttons'>
                         <IconButton
                             Icon={AiOutlinePlus}
-                            className='icon icon--white calendar__content__cells__row__item__action-buttons__button'
-                            // tslint:disable: no-console
-                            onClick={() => console.log('plus')}
+                            className='icon icon--white calendar-cell__action-buttons__button'
+                            onClick={toggleModal}
                         />
                         <IconButton
                             Icon={AiFillEye}
-                            className='icon icon--white calendar__content__cells__row__item__action-buttons__button'
-                            onClick={() => console.log('details')}
+                            className='icon icon--white calendar-cell__action-buttons__button'
                         />
                     </div>
                 );
@@ -38,8 +38,8 @@ export const CalendarCell: React.FC<IProps> = React.memo(
 
         return (
             <div
-                className={cx('calendar__content__cells__row__item', {
-                    'calendar__content__cells__row__item--disabled': disabled,
+                className={cx('calendar-cell', {
+                    'calendar-cell--disabled': disabled,
                 })}
                 onClick={() => onClick(cellDate)}
                 onMouseEnter={useCallback(() => setIsFocus(true), [setIsFocus])}
@@ -47,7 +47,17 @@ export const CalendarCell: React.FC<IProps> = React.memo(
                     setIsFocus,
                 ])}
             >
-                <span className='calendar__content__cells__row__item__date important-info important-info--md'>
+                {showModal && (
+                    <ModalForm
+                        title='Create a personal event'
+                        // tslint:disable-next-line: no-console
+                        onSubmit={() => console.log('test')}
+                        onClose={toggleModal}
+                    >
+                        test
+                    </ModalForm>
+                )}
+                <span className='calendar-cell__date important-info important-info--md'>
                     {format(cellDate, 'd')}
                 </span>
                 {ActionButtons()}
