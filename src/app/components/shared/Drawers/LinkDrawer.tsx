85f6cdcd9d893a95c3301c169655f3c72458856d
agamblin <arthur.gamblin@epitech.eu>
2020-09-25 18:14:59 +0200

    feat(LinkDrawer)
    - added a `history.goBack() onClick event` on drawer overlay

--- src/app/components/shared/Drawers/LinkDrawer.tsx
+++ src/app/components/shared/Drawers/LinkDrawer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation, matchPath } from 'react-router-dom';
+import { useLocation, matchPath, useHistory } from 'react-router-dom';
 import cx from 'classnames';
 import { AnimatedRoute } from 'react-router-transition';
 import { IRouteConfig } from 'app/config';
@@ -9,30 +9,36 @@ interface IProps {
     className?: string;
 }
 
-export const LinkDrawer: React.FC<IProps> = ({ route, className }) => {
-    const location = useLocation();
+export const LinkDrawer: React.FC<IProps> = React.memo(
+    ({ route, className }) => {
+        const location = useLocation();
+        const history = useHistory();
 
-    return (
-        <>
-            <AnimatedRoute
-                path={route.path}
-                component={route.Component}
-                atEnter={{ offset: 100 }}
-                atLeave={{ offset: 100 }}
-                atActive={{ offset: 0 }}
-                mapStyles={styles => ({
-                    transform: `translateX(${styles.offset}%)`,
-                })}
-                className={cx('link-drawer', className, {
-                    'link-drawer--active': matchPath(
-                        location.pathname,
-                        route.path
-                    ),
-                })}
-            />
-            {matchPath(location.pathname, route.path) && (
-                <div className='link-drawer__overlay' />
-            )}
-        </>
-    );
-};
+        return (
+            <>
+                <AnimatedRoute
+                    path={route.path}
+                    component={route.Component}
+                    atEnter={{ offset: 100 }}
+                    atLeave={{ offset: 100 }}
+                    atActive={{ offset: 0 }}
+                    mapStyles={styles => ({
+                        transform: `translateX(${styles.offset}%)`,
+                    })}
+                    className={cx('link-drawer', className, {
+                        'link-drawer--active': matchPath(
+                            location.pathname,
+                            route.path
+                        ),
+                    })}
+                />
+                {matchPath(location.pathname, route.path) && (
+                    <div
+                        className='link-drawer__overlay'
+                        onClick={() => history.goBack()}
+                    />
+                )}
+            </>
+        );
+    }
+);
