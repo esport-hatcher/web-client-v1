9aef78acec3a12849eee10bc9e508e61b51c44b1
Melliti <r.melliti@protonmail.com>
2020-11-22 15:34:41 +0100

    [Add] Solo/duo and fle separation, styles

--- src/app/components/teams/PlayerStats.tsx
+++ src/app/components/teams/PlayerStats.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { IUser } from 'app/actions';
+import darius from 'assets/darius.jpg';
+import jinx from 'assets/jinx.jpg';
+import garen from 'assets/Garen.jpg';
+import leesin from 'assets/lee sin.jpg';
+import wukong from 'assets/Wukong.jpg';
 
 interface IProps {
     item: IUser;
@@ -12,16 +17,57 @@ export const PlayerStats: React.FC<IProps> = ({ item }) => {
                 <h1 className='name'>{item.username}</h1>
             </div>
             <div className='stats__card--container'>
-                <div className='stats__bloc--one'>
-                    <div>
-                        <p className='stats__type--rank'>Rank tmp</p>
+                <div className='stats__card--bloc'>
+                    <h3>Solo/ Duo</h3>
+                    <div className='stats__boxone'>
+                        <div>
+                            <p className='stats__type--rank'>Rank:</p>
+                        </div>
+                        <div>
+                            <p className='stats__type--winrate'>
+                                Win rate: 90%
+                            </p>
+                        </div>
                     </div>
-                    <div>
-                        <p className='stats__type--winrate'>90%</p>
+                    <div className='stats__boxtwo'>
+                        <p className='stats__type--mostplayed'>
+                            Most played heroes:
+                        </p>
+                        <div className='stats__boxtwo--heroes'>
+                            <img src={darius} alt='heros1' />
+                            <img src={jinx} alt='heros2' />
+                            <img src={garen} alt='heros3' />
+                            <img src={leesin} alt='heros4' />
+                            <img src={wukong} alt='heros5' />
+                        </div>
                     </div>
                 </div>
-                <p className='stats__type--mostplayed'>Darius</p>
-                <p className='stats__type--history'>w w l l w</p>
+
+                <div className='stats__card--bloc'>
+                    <h3>Flex</h3>
+                    <div className='stats__boxone'>
+                        <div>
+                            <p className='stats__type--rank'>Rank:</p>
+                        </div>
+                        <div>
+                            <p className='stats__type--winrate'>
+                                Win rate: 90%
+                            </p>
+                        </div>
+                    </div>
+                    <div className='stats__boxtwo'>
+                        <p className='stats__type--mostplayed'>
+                            Most played heroes:
+                        </p>
+                        <div className='stats__boxtwo--heroes'>
+                            <img src={darius} alt='heros1' />
+                            <img src={jinx} alt='heros2' />
+                            <img src={garen} alt='heros3' />
+                            <img src={leesin} alt='heros4' />
+                            <img src={wukong} alt='heros5' />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
